9acd379f7ab0d9fa0ef6bae016f28563f977a2c0
rozpan <rozpan@gmail.com>
2021-02-28 11:44:40 +0200

    another option

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,8 +1,9 @@
 
 //задача_3
 
+
 const hit = document.getElementById("dead"); 
-const miss = document.getElementById("lost"); 
+const miss = document.getElementById("lost");
 
 function getHole(index) {
     return document.getElementById(`hole${index}`);
@@ -28,6 +29,36 @@ for (let index = 1; index <= 9; index++) {
         }
     } 
 }
+
+
+//альтернативное решение кротов. почему она не работает?
+/*
+const holele = document.getElementsByClassName("hole");
+const holeleMole = document.getElementsByClassName("hole hole_has-mole");
+const hit = document.getElementById("dead"); //убито кротов
+const miss = document.getElementById("lost"); // промахов
+console.log(holeleMole);
+console.log(holele);
+
+holele[0].onclick = function() {
+    miss.textContent = Number(miss.textContent) + 1;
+    if (miss.textContent > 5) {
+        alert("GAME OVER");
+        miss.textContent = 0;
+        hit.textContent = 0;
+    }
+}
+
+holeleMole[0].onclick = function() {
+    hit.textContent = Number(hit.textContent) + 1;
+    if (hit.textContent > 10) {
+        alert("ПОБЕДА");
+        hit.textContent = 0;
+        miss.textContent = 0;
+    }
+}
+*/
+
 
 
     
