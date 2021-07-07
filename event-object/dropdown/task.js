45080834b2c8e995fe6c302b0ee0bad7555091c7
rozpan <rozpan@gmail.com>
2021-07-07 22:38:30 +0300

    task_1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,28 +1,19 @@
 
-let krest = document.querySelectorAll(".modal__close_times");
-console.log(krest[0]);
-krest[0].onclick = function() {
-    console.log("rere");
-    let b = krest[0].closest(".modal");
-    b.className = "modal";
+let value = document.querySelector(".dropdown__value"); //текущая основная кнопка
+let list = document.querySelector(".dropdown__list"); //список со вкладками
+let links = document.querySelectorAll(".dropdown__link"); //ссылки все
+
+value.onclick = function() {
+    list.classList.toggle("dropdown__list_active");
 }
-/*
-krest[1].onclick = function() {
-    let b = krest[1].closest(".modal");
-    b.className = "modal";
+
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function() { 
+        return false;
+    }
 }
-*/
-
-
-
-
-let sdelatXorosho = document.querySelector(".show-success");
-console.log(sdelatXorosho);
-let cc = document.getElementById("modal_success");
-let ccc = document.getElementById("modal_main");
-
-sdelatXorosho.onclick = function() {
-    console.log("rere");
-//ccc.className = "modal";
-//cc.className = "modal modal_active";
+  
+list.onclick = function(e) {
+    value.textContent = e.target.textContent;
+    list.classList.remove("dropdown__list_active");
 }
